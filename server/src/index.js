e7ac65a4724802b87a6e6074ad91b6c6d3c96026
Yehor Akunishnikov <yehor_akunishnikov@epam.com>
2021-08-16 22:09:01 +0300

    path import fix

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
